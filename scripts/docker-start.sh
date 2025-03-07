1c700145b10b1158d5bdf4297991466cb760a7d0
Ruben <sneexy@disroot.org>
2025-03-07 09:05:41 -0600

    chore: remove docker script as its no longer necessary

--- scripts/docker-start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-FILE_INDICATOR=/etc/askjs-initialized
-
-if [ ! -e "${FILE_INDICATOR}" ]; then
-    cd /askjs && pnpm migrate
-    touch ${FILE_INDICATOR}
-fi
-
-cd /askjs && pnpm start
